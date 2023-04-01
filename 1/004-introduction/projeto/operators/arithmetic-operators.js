2cd7af36ba68b39e2701f05b4f7f951878f2e2be
Dhyon Keyllo <dhyon.kpm@hotmail.com>
2023-04-01 17:19:23 -0300

    feat: add arithmatic operators examples

--- 1/004-introduction/projeto/operators/arithmetic-operators.js
+++ 1/004-introduction/projeto/operators/arithmetic-operators.js
@@ -14,3 +14,33 @@
     - pre - incremento e decremento - incrementa ou decrementa antes de retornar o valor
     - pos - incremento e decremento - incrementa ou decrementa depois de retornar o valor
 */
+
+// + adição - Soma de Valores
+console.log(2 + 2); /* 4 */
+
+// - subtração - Subtração de Valores
+console.log(2 - 2); /* 0 */
+
+// * multiplicação - Multiplicação de Valores
+console.log(2 * 2); /* 4 */
+
+// / divisão - Divisão de Valores
+console.log(2 / 2); /* 1 */
+
+// % módulo - Resto da Divisão de Valores
+console.log(2 % 2); /* 0 */
+
+// ** exponenciação - Potência de Valores (ES2016)
+console.log(2 ** 2); /* 4 */
+
+// ++ incremento - Incrementa o Valor em 1 (pré-incremento e pós-incremento)
+let nIncrement = 1;
+console.log(++n); /* 2 */
+console.log(n++); /* 2 */
+console.log(n); /* 3 */
+
+// -- decremento - Decrementa o Valor em 1 (pré-decremento e pós-decremento)
+let nDecrement = 1;
+console.log(--n); /* 0 */
+console.log(n--); /* 0 */
+console.log(n); /* 1 */
